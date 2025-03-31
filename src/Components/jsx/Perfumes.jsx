235795ae5034e9a099e913ce50e2846e9502ba65
adriiberjaga <adriabdd@gmail.com>
2025-03-31 12:51:57 +0200

    .

--- src/Components/jsx/Perfumes.jsx
+++ src/Components/jsx/Perfumes.jsx
@@ -7,8 +7,9 @@ export default function Perfumes() {
     <div className="perfumes-content">
       {Perfumess.map((p) => (
         <div className="perfumes-content__box" key={p.id}>
+          <img src={p.img} alt="" />
           <p>{p.title}</p>
-          <p>{p.id}</p>
+          <div>{p.price} $</div>
         </div>
       ))}
     </div>
--- src/content/Perfumes.js
+++ src/content/Perfumes.js
@@ -2,21 +2,29 @@ export const Perfumess = [
     {
         title: 'titulo 1',
         id: crypto.randomUUID(),
-        desc: 'descripcion'
+        desc: 'descripcion',
+        img: 'https://picsum.photos/200/300',
+        price: 34
     },
     {
         title: 'titulo 2',
         id: crypto.randomUUID(),
-        desc: 'descripcion'
+        desc: 'descripcion',
+        img: 'https://picsum.photos/200/300',
+        price: 34
     },
     {
         title: 'titulo 3',
         id: crypto.randomUUID(),
-        desc: 'descripcion'
+        desc: 'descripcion',
+        img: 'https://picsum.photos/200/300',
+        price: 34
     },
     {
         title: 'titulo 4',
         id: crypto.randomUUID(),
-        desc: 'descripcion'
+        desc: 'descripcion',
+        img: 'https://picsum.photos/200/300',
+        price: 34
     },
 ]
